c06f8056af8939804b053ffbe99b5120e8a513cd
Claudio Cambra <developer@claudiocambra.com>
2024-04-02 17:03:08 +0800

    Add basic suggestion type

---
+++ Sources/SuggestionsTextFieldKit/Suggestion.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Claudio Cambra on 2/4/24.
+//
+
+import Foundation
+
+struct Suggestion {
+    let imageName: String
+    let displayText: String
+    let data: Any
+}
