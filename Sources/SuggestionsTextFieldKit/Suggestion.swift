be4b78aa629b04b4c04116f3af1d1286ab915f6e
Claudio Cambra <developer@claudiocambra.com>
2024-04-02 18:13:34 +0800

    add public init to suggestion

--- Sources/SuggestionsTextFieldKit/Suggestion.swift
+++ Sources/SuggestionsTextFieldKit/Suggestion.swift
@@ -11,4 +11,10 @@ public struct Suggestion {
     public let imageName: String
     public let displayText: String
     public let data: Any
+
+    public init(imageName: String, displayText: String, data: Any) {
+        self.imageName = imageName
+        self.displayText = displayText
+        self.data = data
+    }
 }
