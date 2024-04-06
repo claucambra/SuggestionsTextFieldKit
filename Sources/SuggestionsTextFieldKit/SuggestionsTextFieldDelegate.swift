50b58bbdb15f31d572055f1e0a64ecc0a0ec26a2
Claudio Cambra <developer@claudiocambra.com>
2024-04-06 19:02:59 +0800

    Handle window controller from within suggestions text field delegate

--- Sources/SuggestionsTextFieldKit/SuggestionsTextFieldDelegate.swift
+++ Sources/SuggestionsTextFieldKit/SuggestionsTextFieldDelegate.swift
@@ -9,11 +9,36 @@ import AppKit
 import Foundation
 
 public class SuggestionsTextFieldDelegate: NSObject, NSTextFieldDelegate {
-    public var suggestionsWindowController: SuggestionsWindowController?
+    public var suggestionsWindowController: SuggestionsWindowController? {
+        didSet {
+            suggestionsWindowController?.parentTextField = targetTextField
+            suggestionsWindowController?.dataSource = suggestionsDataSource
+            suggestionsWindowController?.selectionHandler = selectionHandler
+            suggestionsWindowController?.confirmationHandler = confirmationHandler
+        }
+    }
+    public var suggestionsDataSource: SuggestionsDataSource? {
+        didSet { suggestionsWindowController?.dataSource = suggestionsDataSource }
+    }
+    public var targetTextField: NSTextField? {
+        didSet { suggestionsWindowController?.parentTextField = targetTextField }
+    }
+    public var selectionHandler: (@Sendable (Suggestion?) -> ())? {
+        didSet { suggestionsWindowController?.selectionHandler = selectionHandler }
+    }
+    public var confirmationHandler: (@Sendable (Suggestion?) -> ())? {
+        didSet { suggestionsWindowController?.confirmationHandler = confirmationHandler }
+    }
 
     public func controlTextDidBeginEditing(_ notification: Notification) {
         guard let control = notification.object as? NSControl else { return }
+
+        if suggestionsWindowController == nil {
+            suggestionsWindowController = SuggestionsWindowController()
+        }
+
         suggestionsWindowController?.dataSource?.inputString = control.stringValue
+
         if !control.stringValue.isEmpty, suggestionsWindowController?.window?.isVisible == false {
             suggestionsWindowController?.showWindow(self)
         }
